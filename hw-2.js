a4aaaca052e40a63a9c410736877ac8d740b50a7
Aleksey8622 <aleksey.rudnev.ru@gmail.com>
2023-05-12 19:38:46 +0300

    file changed

--- hw-2.js
+++ hw-2.js
@@ -1,27 +1,51 @@
 // Задание 1
-let a;
-a = 10;
-a = 20;
-alert(a);
+// let a;
+// a = 10;
+// a = 20;
+// alert(a);
 
 // Задание 2
-let year = 2007;
-alert(year);
+// let year = 2007;
+// alert(year);
 
 // Задание 3
-let creator = "Брендан Эйх";
-alert(creator);
+// let creator = "Брендан Эйх";
+// alert(creator);
 
 // Задание 4
-let ten = 10;
-let two = 2;
-alert(ten + two);
-alert(ten - two);
-alert(ten * two);
-alert(ten / two);
+// let ten = 10;
+// let two = 2;
+// alert(ten + two);
+// alert(ten - two);
+// alert(ten * two);
+// alert(ten / two);
 
 // Задание 5
-let result = 2 ** 5;
-alert(result);
+// let result = 2 ** 5;
+// alert(result);
 
 // Задание 6
+// let a = 9;
+// let b = 2;
+// alert(a % b);
+
+// Задание 7
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num += 1;
+// num -= 1;
+// alert(num);
+
+// Задание 8
+// let age = prompt('Сколько вам лет?');
+// alert(age);
+
+// Задание 9.0
+// const user = {
+//     name: 'Виктория',
+//     age: 5,
+//     isAdmin: true,
+// }
