66a71fca5468b1ce3444cd5b9b7219be53e2557f
Aleksey8622 <aleksey.rudnev.ru@gmail.com>
2023-05-12 19:07:32 +0300

    new file

--- hw-2.js
+++ hw-2.js
@@ -3,3 +3,19 @@ let a;
 a = 10;
 a = 20;
 alert(a);
+
+// Задание 2
+let year = 2007;
+alert(year);
+
+// Задание 3
+let creator = "Брендан Эйх";
+alert(creator);
+
+// Задание 4
+let ten = 10;
+let two = 2;
+alert(ten + two);
+alert(ten - two);
+alert(ten * two);
+alert(ten / two);
