fa20a9e651a7b1fe8014757d3f3ce9b9d50cd6ee
Aleksey8622 <aleksey.rudnev.ru@gmail.com>
2023-05-16 21:08:00 +0300

    change file

---
+++ hw-3.js
@@ -0,0 +1,71 @@
+// Домашнее задание 2.3 "Условное ветвление"
+
+
+// Задание 1
+// let password = 'пароль';
+// let question = prompt('Введите пароль');
+// (question === password) ? console.log('Пароль введен верно') : console.log('Пароль введен неправильно');
+
+// Задание 2
+// let input = Number(prompt('Введите любое число!'));
+// let number = input;
+// (number > 0 && number < 10) ? console.log('Верно') : console.log('Неверно');
+
+// Задание 3
+// let d = Number(prompt('Введите число'));
+// let e = Number(prompt('Введите число'));
+// if (d > 100 || e > 100) {
+//     console.log('Верно');
+// } else if (d < 100 || e < 100) {
+//     console.log('Неверно');
+// }
+
+// Задание 4
+// let a = '2';
+// let b = '3';
+// alert(Number(a + b));
+
+
+// Задание 5
+// let monthNumber = Number(prompt('Введите номер месяца!'));
+// switch (monthNumber) {
+//     case 1:
+//         console.log('Месяц январь');
+//         break;
+//     case 2:
+//         console.log('Месяц февраль');
+//         break;
+//     case 3:
+//         console.log('Месяц март');
+//         break;
+//     case 4:
+//         console.log('Месяц апрель');
+//         break;
+//     case 5:
+//         console.log('Месяц май');
+//         break;
+//     case 6:
+//         console.log('Месяц июнь');
+//         break;
+//     case 7:
+//         console.log('Месяц июль');
+//         break;
+//     case 8:
+//         console.log('Месяц август');
+//         break;
+//     case 9:
+//         console.log('Месяц сентябрь');
+//         break;
+//     case 10:
+//         console.log('Месяц октябрь');
+//         break;
+//     case 11:
+//         console.log('Месяц ноябрь');
+//         break;
+//     case 12:
+//         console.log('Месяц декабрь');
+//         break;
+//     default:
+//         console.log('Введено не верное значение');
+//         break;
+// }
