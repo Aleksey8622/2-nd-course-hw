971518e050faec8f1013fa3a5be9f903e7dae5f8
Aleksey8622 <121502314+Aleksey8622@users.noreply.github.com>
2023-05-20 23:09:43 +0300

    change new file hw-4.js

---
+++ hw-4.js
@@ -0,0 +1,59 @@
+// Домашняя работа 2.4
+
+// Задание 1
+
+// let word = 0;
+// while (word <= 1) {
+//     console.log('Привет');
+//     word++;
+    
+// }
+
+// Задание 2
+
+// let num = 1;
+// while (num <= 5) {
+//     console.log(num);
+//     num++;
+    
+// }
+
+// Задагние 3
+
+// let num = 7;
+// while (num <= 22) {
+//     console.log(num);
+//     num++;
+// }
+
+// Задание 4
+
+// const obj = {
+//     'Коля': '200',
+//     'Вася': '300',
+//     'Петя': '400'
+// }
+// for (let name in obj){
+//    console.log(`${name} - зарплата ${obj[name]} долларов`); 
+// } 
+
+// Задание 5
+
+// let n = 1000;
+// num = 0;
+// while (n >= 50) {
+//     n /= 2;
+//     num++;
+// }
+// console.log(n); 
+
+// Задание 6
+
+// for (let friday = 5; friday <= 31; friday++) {
+//     if (friday == 5 || friday == 12 || friday == 19 || friday == 26) {
+//         console.log(`Сегодня ${friday}-число. Необходимо подготовить отчет.`);
+//     }
+  
+// }
+
+    
