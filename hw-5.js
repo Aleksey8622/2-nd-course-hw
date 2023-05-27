c7ed2bdd288f73b7b446bc258abe0732a8e6c6a2
Aleksey8622 <121502314+Aleksey8622@users.noreply.github.com>
2023-05-27 20:24:14 +0300

    Added 8 tasks to the file hw-5.js

--- hw-5.js
+++ hw-5.js
@@ -117,4 +117,46 @@
 // }
 
 // console.log(circle.getArea());
-// console.log(circleTwo.getPerimeter());config
+// console.log(circleTwo.getPerimeter());
+
+// Задание 8
+
+// Вариант 1
+
+// const seasons = (num) => {
+
+
+//     do {
+//         num = prompt("Введите месяц");
+//         if (num == 12 || num <= 2) {
+//             console.log('Время года Зима!');
+//         } else if (num <= 3 || num <= 5) {
+//             console.log('Время года Весна!');
+//         } else if (num <= 6 || num <= 8) {
+//             console.log('Время года Лето!');
+//         } else if (num <= 9 || num <= 11) {
+//             console.log('Время года Осень!');
+//         } else {
+//             console.log('Этого времени года еще не придумали! Конец игре!');
+//         }
+//     } while (num <= 12);
+
+// }
+
+// Вариант 2
+
+// do {
+//     num = prompt("Введите месяц");
+//     if (num == 12 || num <= 2) {
+//         alert('Время года Зима!');
+//     } else if (num <= 3 || num <= 5) {
+//         alert('Время года Весна!');
+//     } else if (num <= 6 || num <= 8) {
+//         alert('Время года Лето!');
+//     } else if (num <= 9 || num <= 11) {
+//         alert('Время года Осень!');
+//     } else {
+//         alert('Этого времени года еще не придумали! Конец игре!');
+//     }
+// } while (num <= 12);
+
