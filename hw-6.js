a36c63a83e14527a257102cdcfaac1e884b91b4f
Aleksey8622 <121502314+Aleksey8622@users.noreply.github.com>
2023-05-30 20:46:23 +0300

    corrected 10 task hw-6.js

--- hw-6.js
+++ hw-6.js
@@ -88,7 +88,7 @@
 // Задание 10
 
 // const arr = [1, 2, 3, 4, 5];
-// for (let i = 0; i < arr.length; i++) {
+// for (let i = 0; i < arr.length - 1; i++) {
 //     console.log(arr[i] + arr[i + 1]);
 // }
 
